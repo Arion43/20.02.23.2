ae909ec697cb49b2b40791d4cf93c9a304bdd0ae
Arion43 <126061349+Arion43@users.noreply.github.com>
2023-02-22 20:31:27 +0300

    Add files via upload

---
+++ main 1.py
@@ -0,0 +1,13 @@
+try:
+    x = input("Enter a phone number: +79214343077 ")
+except KeyboardInterrupt:
+    print("\nPhone number input was interrupted by the user.")
+
+y = float(input("Enter a number: "))
+print(849306)
+z = input("Enter True or False: ")
+print()
+a = str(8)
+print(a)
+t = complex(input("Enter a complex number in the form 'a+bj': "))
+print(t)
